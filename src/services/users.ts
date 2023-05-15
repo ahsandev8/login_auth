a777faf41b5b110aeab1d966a16813de531ebf20
ahsandev8 <ahsan.dev.8@gmail.com>
2023-05-16 01:16:31 +0530

    users service

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,5 +1,7 @@
 import { UserModel } from "../model/users";
 
+
+
 export const getUsers = () => UserModel.find();
 
 export const getUserByEmail = (email: string) => UserModel.findOne({ email });
@@ -7,6 +9,7 @@ export const getUserByEmail = (email: string) => UserModel.findOne({ email });
 export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
     'auth.sessionToken': sessionToken
 });
+
 
 export const getUserById = (id: String) => UserModel.findById(id);
 
